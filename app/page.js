6ac27fb5fb0ee3069bb7bd7c7c1495a3a0fc2f48
Julia <jujubafreitasnog@gmail.com>
2025-11-26 12:50:13 -0300

    Descrição adicionada na Página Principal

--- app/page.js
+++ app/page.js
@@ -6,7 +6,11 @@ function Home() {
   
         <h1>Bem-vindo ao  Adota Pet</h1>
   
-        <p>Adote animais de estimação e faça doações!</p>
+        <p>Adote animais de estimação e faça doações!
+        Aqui, você descobre animais resgatados que estão prontos 
+        para dar e receber muito carinho. Navegue pelos perfis, 
+        conheça suas histórias e adote com responsabilidade.
+        </p>
   
       </div>
   
